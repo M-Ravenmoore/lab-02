a6f1d2cfe34494898c617a540cc84185c76ba977
Matt Hudson <m_ravenmoore@outlook.com>
2020-08-05 20:54:49 -0700

    flushed out code on js and html css typo fixes

--- js/app.js
+++ js/app.js
@@ -57,4 +57,4 @@ if (question5.toLowerCase() === 'yes' || question5.toLowerCase() === 'y'){
 
 
 alert('Welcome ' + userName + ', to my about me game! The results of your quiz are as follows');
-alert('Question 1: ' + answer1 + ' Question 2: ' + answer2 + ' Question 3: ' + answer3 + ' Question 4: ' + answer4 + ' Question 5: ' + answer5 + '.  How Did You Do?');
+alert('Question 1: Is the Sky blue?' + answer1 + ' Question 2: Do woodchucks chuck wood?' + answer2 + ' Question 3: Gravity works Yes or no?' + answer3 + ' Question 4: will the Cyote ever catch The road runner?' + answer4 + ' Question 5: The world is Flat yes or no? ' + answer5 + '.  How Did You Do?');
